98cf23fd3d585e1c0c34c413bd70732401cf2737
Josh Comer <joshjcomer@gmail.com>
2011-08-08 20:25:41 -0300

    Finally got some coloured output

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A Clojure wrapper for jANSI"
   :dependencies [[org.clojure/clojure "1.2.1"]
   				 [org.fusesource.jansi/jansi "1.6"]
-  				 [org.clojure/clojure-contrib "1.2.0"]])
+  				 [org.clojure/clojure-contrib "1.2.0"]]
+  :main clJansi.core)
--- src/clJansi/core.clj
+++ src/clJansi/core.clj
@@ -1,3 +1,18 @@
 (ns clJansi.core
 	(:use [clojure.contrib.import-static])
-	(:import [org.fusesource.jansi AnsiConsole]))
+	(:import [org.fusesource.jansi AnsiConsole AnsiRenderWriter AnsiRenderer])
+	(:gen-class))
+
+(import-static org.fusesource.jansi.AnsiConsole systemInstall)
+(import-static org.fusesource.jansi.AnsiRenderer render)
+(def writer (AnsiRenderWriter. *out* true))
+
+(defn print-ansi
+	[text]
+	(.write writer (render text)))
+	;(.println org.fusesource.jansi.AnsiConsole/out (render text)))
+
+(defn -main [& args]
+	(systemInstall)
+	(print-ansi "@|red Hello|@ @|green World|@"))
+	;(println (.. org.fusesource.jansi.Ansi (ansi) (render "@|red Hello|@ @|green World|@") (toString))))
